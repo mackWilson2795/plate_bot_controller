e891fef852526e7dc6d54555546e2fa066bb9ebd
Ethan4416 <predinchuk.e@outlook.com>
2022-11-16 19:44:50 -0800

    Rudementary controller is working, still needs to start competition timer and implement a state machine

--- scripts/driver.py
+++ scripts/driver.py
@@ -1,8 +1,48 @@
 #! /usr/bin/env python3
 
+import sys
 import rospy
+import cv2
+from std_msgs.msg import String
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
 from geometry_msgs.msg import Twist
+import time
 
+class comp_driver:
 
-rospy.init_node('topic_publisher')
-print("WE DID IT")
+    def __init__(self):
+        self.mover = rospy.Publisher("/R1/cmd_vel",
+                                        Twist,
+                                        queue_size = 1)
+        self.bridge = CvBridge()
+        self.image_sub = rospy.Subscriber("/R1/pi_camera/image_raw",
+                                            Image,
+                                            self.callback)
+        time.sleep(1)
+
+    def callback(self,data):
+        try:
+            cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+        except CvBridgeError as e:
+            print(e) #this could get changed to pass so that we don't clog the terminal during comp if there is no image
+         
+        move = Twist()
+        move.linear.x = 0.1
+        move.angular.z = 0.8
+
+        cv2.imshow("raw feed", cv_image)
+        cv2.waitKey(3)
+
+        try:
+            self.mover.publish(move)
+        except CvBridgeError as e:
+            print(e)
+
+rospy.init_node('comp_driver', anonymous = True)
+comp = comp_driver()
+try:
+    rospy.spin()
+except KeyboardInterrupt:
+    print("Shutting Down")
+cv2.destroyAllWindows()
